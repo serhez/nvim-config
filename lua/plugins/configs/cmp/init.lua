f65316699016734c84abb73a6a03ffb6f4797a59
Sergio Hernandez <ser@Sergios-MacBook-Pro.local>
2022-06-13 17:45:01 +0100

    Removed unused cmp mappings

--- lua/plugins/configs/cmp/init.lua
+++ lua/plugins/configs/cmp/init.lua
@@ -6,6 +6,11 @@ end
 
 local luasnip_present, luasnip = pcall(require, "luasnip")
 
+local has_words_before = function()
+    local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
     -- Disable for comments
     enabled = function ()
@@ -47,8 +52,6 @@ cmp.setup({
         end,
     },
     mapping = {
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
@@ -62,6 +65,8 @@ cmp.setup({
                 cmp.select_next_item()
             elseif luasnip_present and luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
             else
                 fallback()
             end

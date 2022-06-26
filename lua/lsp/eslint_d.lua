ef956e67fca6497e575b351b0cee1f1d25c90244
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2022-06-26 20:58:18 +0100

    Lsp eslint config

--- lua/lsp/eslint_d.lua
+++ lua/lsp/eslint_d.lua
@@ -1,6 +1,13 @@
 local settings = {
     -- Do not use for Vue (use volar instead)
-    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+    filetypes = {
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+    },
 
     settings = {
         -- Do not use for formatting, use prettierd instead (through null-ls)

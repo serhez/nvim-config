5eac1d7b5adcb71ed308623dbc4dc6fb4d460666
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2022-07-17 21:57:25 +0100

    Fix winbar file icon

--- lua/plugins/configs/incline/init.lua
+++ lua/plugins/configs/incline/init.lua
@@ -2,10 +2,12 @@ local icons = require("icons")
 
 require("incline").setup({
     render = function()
-        return require("feline").providers.file_info({}, {
+        local name, icon = require("feline").providers.file_info({}, {
             type = "base-only",
             file_readonly_icon = icons.lock .. icons.single_space,
         })
+
+        return string.format("%s%s", icon.str, name)
     end,
     window = {
         margin = {

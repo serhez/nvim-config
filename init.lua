49456e7684e329baf12ecff19ba2edc356fe8a68
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2022-08-19 20:30:38 +0100

    fix: load color settings after plugins

--- init.lua
+++ init.lua
@@ -1,17 +1,15 @@
-local sum = require("lsp.servers.sumneko_lua")
-
 local present, impatient = pcall(require, "impatient")
 
 if present then
-	impatient.enable_profile()
+    impatient.enable_profile()
 end
 
 require("settings")
-require("colors")
 require("plugins")
+require("colors")
 require("mappings")
 require("autocommands")
 
 if vim.g.neovide then
-	require("neovide")
+    require("neovide")
 end

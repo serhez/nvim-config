1bed9935fd842514285ebfbd4bac9a433c5b14b5
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-02-15 14:47:38 +0200

    feat: remove markdown-preview and project.nvim

--- lua/plugins/configs/quarto.lua
+++ lua/plugins/configs/quarto.lua
@@ -13,9 +13,6 @@ function M.init()
 	mappings.register_normal({
 		m = {
 			name = "Markdown",
-			a = { "<cmd>QuartoActivate<cr>", "Activate quarto" },
-			d = { "<cmd>QuartoDiagnostics<cr>", "Diagnostics" },
-			h = { "<cmd>QuartoHover<cr>", "Hover" },
 			p = { "<cmd>QuartoPreview<cr>", "Preview" },
 			P = { "<cmd>QuartoClosePreview<cr>", "Close preview" },
 		},

c931ef5f25f3d385bb4d08cd67c2a9cf352b1c62
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-02-15 14:12:36 +0200

    feat: remove nvim-notify

--- lua/plugins/configs/noice.lua
+++ lua/plugins/configs/noice.lua
@@ -5,8 +5,6 @@ local M = {
 	dependencies = {
 		-- If you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
-		-- `nvim-notify` is only needed, if you want to use the notification view.
-		"rcarriga/nvim-notify",
 	},
 	event = "VeryLazy",
 }
@@ -47,8 +45,8 @@ function M.config()
 		messages = {
 			enabled = true, -- enables the Noice messages UI
 			view = "mini", -- default view for messages
-			view_error = "notify", -- view for errors
-			view_warn = "notify", -- view for warnings
+			view_error = "mini", -- view for errors
+			view_warn = "mini", -- view for warnings
 			view_history = "messages", -- view for :messages
 			view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
 		},

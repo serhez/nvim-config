a10d608f1e4c234cbcb1c992d15b29d36cc22293
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-05-16 15:54:00 +0300

    feat: silence "No information" message for LSP hover

--- lua/plugins/configs/mason-lspconfig/init.lua
+++ lua/plugins/configs/mason-lspconfig/init.lua
@@ -126,6 +126,7 @@ function M.config()
 	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 		border = "single",
 		position = { row = 2, col = 2 },
+		silent = true,
 	})
 	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
 		border = "single",
--- lua/plugins/configs/noice.lua
+++ lua/plugins/configs/noice.lua
@@ -64,9 +64,21 @@ function M.config()
 		},
 
 		lsp = {
+			override = {
+				-- override the default lsp markdown formatter with Noice
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				-- override the lsp markdown formatter with Noice
+				["vim.lsp.util.stylize_markdown"] = true,
+				-- override cmp documentation with Noice (needs the other options to work)
+				["cmp.entry.get_documentation"] = true,
+			},
 			message = {
 				enabled = true,
 				view = "mini",
+			},
+			hover = {
+				enabled = true,
+				silent = true,
 			},
 		},
 

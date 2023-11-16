bfa7dc22a60d9f5752565b1d496deffed25a35ed
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2023-11-16 17:26:34 +0200

    feat: wordmotion -> spider

---
+++ lua/plugins/configs/spider.lua
@@ -0,0 +1,14 @@
+local M = {
+	"chrisgrieser/nvim-spider",
+	event = "CursorMoved",
+}
+
+function M.config()
+	-- Keymaps
+	vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+	vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+	vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
+	vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
+end
+
+return M

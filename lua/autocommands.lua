f7a7d4cba34d2451feadb5b68a2d93021f3c2166
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-01-07 17:06:38 +0000

    feat: new autocommands

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -60,6 +60,55 @@ function M.setup()
 			{ "FileType", "floaterm", "nnoremap <silent> <buffer> q :q<CR>" },
 		},
 	})
+
+	-- Close some filetypes with <q>
+	vim.api.nvim_create_autocmd("FileType", {
+		group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+		pattern = {
+			"PlenaryTestPopup",
+			"help",
+			"lspinfo",
+			"man",
+			"notify",
+			"qf",
+			"query",
+			"spectre_panel",
+			"startuptime",
+			"tsplayground",
+			"neotest-output",
+			"checkhealth",
+			"neotest-summary",
+			"neotest-output-panel",
+		},
+		callback = function(event)
+			vim.bo[event.buf].buflisted = false
+			vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+		end,
+	})
+
+	-- From LazyVim
+	-- Wrap and check for spell in text filetypes
+	vim.api.nvim_create_autocmd("FileType", {
+		group = vim.api.nvim_create_augroup("wrap_spell", { clear = true }),
+		pattern = { "gitcommit", "markdown" },
+		callback = function()
+			vim.opt_local.wrap = true
+			vim.opt_local.spell = true
+		end,
+	})
+
+	-- From LazyVim
+	-- Auto create dir when saving a file, in case some intermediate directory does not exist
+	vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+		group = vim.api.nvim_create_augroup("auto_create_dir", { clear = true }),
+		callback = function(event)
+			if event.match:match("^%w%w+://") then
+				return
+			end
+			local file = vim.loop.fs_realpath(event.match) or event.match
+			vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+		end,
+	})
 end
 
 return M

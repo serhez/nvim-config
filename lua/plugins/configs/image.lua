cff525cbaf03bd75274d2d5a6e330afc9e1dafb9
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-01-09 12:38:55 +0000

    fix: do not use image.nvim with neovide

--- lua/plugins/configs/image.lua
+++ lua/plugins/configs/image.lua
@@ -1,7 +1,7 @@
 local M = {
 	"3rd/image.nvim",
 	ft = { "markdown", "quarto", "rmd", "ipynb" },
-	cond = not vim.g.started_by_firenvim and not vim.g.vscode,
+	cond = not vim.g.started_by_firenvim and not vim.g.vscode and not vim.g.neovide,
 }
 
 function M.init()

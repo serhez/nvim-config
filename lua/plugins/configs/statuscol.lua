8d1f092112f425cf5eaa8510830fa0321ef1d88e
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2024-01-25 22:41:13 +0200

    feat: no todo's in statuscolumn

--- lua/plugins/configs/statuscol.lua
+++ lua/plugins/configs/statuscol.lua
@@ -18,22 +18,26 @@ function M.config()
 		setopt = true, -- whether to set the 'statuscolumn', providing builtin click actions
 		clickmod = "c", -- modifier used for certain actions: "a" for Alt, "c" for Ctrl and "m" for Meta
 		segments = {
-			-- {
-			-- 	sign = { namespace = { "diagnostic" }, colwidth = 1 },
-			-- 	click = "v:lua.ScSa",
-			-- },
-			-- {
-			-- 	sign = { name = { "todo*" }, colwidth = 1 },
-			-- },
 			{
 				sign = {
-					name = { ".*" },
-					text = { ".*" },
-					colwidth = 1, -- with `colwidth = 2`, we get a space between each sign, but the statusline is too wide
-					maxwidth = 2,
+					namespace = { "diagnostic" },
+					colwidth = 1,
+					maxwidth = 1,
 				},
 				click = "v:lua.ScSa",
 			},
+			-- {
+			-- 	sign = { name = { "todo*" }, colwidth = 1 },
+			-- },
+			-- {
+			-- 	sign = {
+			-- 		name = { ".*" },
+			-- 		text = { ".*" },
+			-- 		colwidth = 1, -- with `colwidth = 2`, we get a space between each sign, but the statusline is too wide
+			-- 		maxwidth = 2,
+			-- 	},
+			-- 	click = "v:lua.ScSa",
+			-- },
 			{
 				sign = { name = { "Dap*" }, colwidth = 1 },
 			},

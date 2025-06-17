70f62475e30a53fbadd25c58b3d92588372c73f5
Sergio Hernández Gutiérrez <ser@u-082-c240.eap.uni-tuebingen.de>
2025-06-17 13:48:25 +0200

    feat(md): more appropriate header icons

--- lua/plugins/configs/render-markdown.lua
+++ lua/plugins/configs/render-markdown.lua
@@ -75,12 +75,12 @@ function M.config()
 			-- The 'level' is used to index into the array using a cycle
 			-- The result is left padded with spaces to hide any additional '#'
 			icons = {
-				block .. " " .. icons.numbers.circled_filled[1] .. " ",
-				block .. block .. " " .. icons.numbers.circled_filled[2] .. " ",
-				block .. block .. block .. " " .. icons.numbers.circled_filled[3] .. " ",
-				block .. block .. block .. block .. " " .. icons.numbers.circled_filled[4] .. " ",
-				block .. block .. block .. block .. block .. " " .. icons.numbers.circled_filled[5] .. " ",
-				block .. block .. block .. block .. block .. block .. " " .. icons.numbers.circled_filled[6] .. " ",
+				block .. " " .. icons.numbers.header[1] .. " ",
+				block .. block .. " " .. icons.numbers.header[2] .. " ",
+				block .. block .. block .. " " .. icons.numbers.header[3] .. " ",
+				block .. block .. block .. block .. " " .. icons.numbers.header[4] .. " ",
+				block .. block .. block .. block .. block .. " " .. icons.numbers.header[5] .. " ",
+				block .. block .. block .. block .. block .. block .. " " .. icons.numbers.header[6] .. " ",
 			},
 			-- icons = { "", "", "", "", "", "" },
 			-- Highlight for the heading icon and extends through the entire line

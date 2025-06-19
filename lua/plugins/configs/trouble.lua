fb1d35645b181dc5e0b915b91952f68ac12d27fc
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2025-06-19 23:54:38 +0200

    feat(trouble): better qfl & preview

--- lua/plugins/configs/trouble.lua
+++ lua/plugins/configs/trouble.lua
@@ -14,6 +14,16 @@ function M.init()
 	vim.api.nvim_del_keymap("n", "gri")
 	vim.api.nvim_del_keymap("n", "gO")
 
+	-- Override quickfile and location list mappings
+	vim.api.nvim_del_keymap("n", "[q")
+	vim.api.nvim_del_keymap("n", "]q")
+	vim.api.nvim_del_keymap("n", "[Q")
+	vim.api.nvim_del_keymap("n", "]Q")
+	vim.api.nvim_del_keymap("n", "[l")
+	vim.api.nvim_del_keymap("n", "]l")
+	vim.api.nvim_del_keymap("n", "[L")
+	vim.api.nvim_del_keymap("n", "]L")
+
 	require("mappings").register({
 		-- x = {
 		-- 	"<cmd>Trouble mode=lsp_document_symbols pinned=true win.position=right win.relative=win auto_preview=false<cr>",
@@ -27,6 +37,72 @@ function M.init()
 		{ "gt", "<cmd>Trouble lsp_type_definitions<cr>", desc = "Type definitions" },
 		{ "gI", "<cmd>Trouble lsp_incoming_calls<cr>", desc = "Incoming calls" },
 		{ "gO", "<cmd>Trouble lsp_outgoing_calls<cr>", desc = "Outgoing calls" },
+
+		-- Quickfix and location list
+		{
+			"[n",
+			function()
+				require("trouble").prev("qflist")
+				require("trouble").jump_only("qflist")
+			end,
+			desc = "Previous quickfix list item",
+		},
+		{
+			"]n",
+			function()
+				require("trouble").next("qflist")
+				require("trouble").jump_only("qflist")
+			end,
+			desc = "Next quickfix list item",
+		},
+		{
+			"[N",
+			function()
+				require("trouble").first("qflist")
+				require("trouble").jump_only("qflist")
+			end,
+			desc = "First quickfix list item",
+		},
+		{
+			"]N",
+			function()
+				require("trouble").last("qflist")
+				require("trouble").jump_only("qflist")
+			end,
+			desc = "Last quickfix list item",
+		},
+		{
+			"[n",
+			function()
+				require("trouble").prev("loclist")
+				require("trouble").jump_only("loclist")
+			end,
+			desc = "Previous location list item",
+		},
+		{
+			"]n",
+			function()
+				require("trouble").next("loclist")
+				require("trouble").jump_only("loclist")
+			end,
+			desc = "Next location list item",
+		},
+		{
+			"[N",
+			function()
+				require("trouble").first("loclist")
+				require("trouble").jump_only("loclist")
+			end,
+			desc = "First location list item",
+		},
+		{
+			"]N",
+			function()
+				require("trouble").last("loclist")
+				require("trouble").jump_only("loclist")
+			end,
+			desc = "Last location list item",
+		},
 
 		{ "<leader>cd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Diagnostics (buffer)" },
 		{
@@ -56,37 +132,52 @@ function M.init()
 	-- Hijack quickfix and location list
 	-- We still want to avoid calling the original commands as much as possible,
 	-- in order to avoid the overhead of closing and reopening the window
-	-- vim.api.nvim_create_autocmd("BufRead", {
-	-- 	callback = function(ev)
-	-- 		if vim.bo[ev.buf].buftype == "quickfix" then
-	-- 			vim.schedule(function()
-	-- 				vim.cmd([[cclose]])
-	-- 				require("trouble").open({ mode = "qflist" })
-	-- 			end)
-	-- 		elseif vim.bo[ev.buf].buftype == "loclist" then
-	-- 			vim.schedule(function()
-	-- 				vim.cmd([[lclose]])
-	-- 				require("trouble").open({ mode = "loclist" })
-	-- 			end)
-	-- 		end
-	-- 	end,
-	-- })
+	vim.api.nvim_create_autocmd("BufRead", {
+		callback = function(ev)
+			if vim.bo[ev.buf].buftype == "quickfix" then
+				vim.schedule(function()
+					vim.cmd([[cclose]])
+					require("trouble").open({ mode = "qflist" })
+				end)
+			elseif vim.bo[ev.buf].buftype == "loclist" then
+				vim.schedule(function()
+					vim.cmd([[lclose]])
+					require("trouble").open({ mode = "loclist" })
+				end)
+			end
+		end,
+	})
 end
 
 function M.config()
 	require("trouble").setup({
-		pinned = false, -- When pinned, the opened trouble window will be bound to the current buffer
+		auto_close = false, -- auto close when there are no items
+		auto_open = false, -- auto open when there are items
+		auto_preview = true, -- automatically open preview when on an item
+		auto_refresh = true, -- auto refresh when open
+		auto_jump = true, -- auto jump to the item when there's only one
 		focus = true, -- Focus the window when opened
-		follow = true, -- Follow the current item
-		auto_jump = true,
-		auto_refresh = false,
+		restore = true, -- restores the last location in the list when opening
+		follow = false, -- Follow the current item
+		indent_guides = true, -- show indent guides
+		max_items = 500, -- limit number of items that can be displayed per section
+		multiline = true, -- render multi-line messages
+		pinned = true, -- When pinned, the opened trouble window will be bound to the current buffer
 		warn_no_results = true, -- show a warning when there are no results
-		open_no_results = false,
+		open_no_results = false, -- open the trouble window when there are no results
 		preview = {
-			type = "main",
+			-- type = "split",
 			-- relative = "win",
 			-- position = "right",
 			-- size = 0.4,
+			type = "float",
+			relative = "editor",
+			border = "solid",
+			title = "Preview",
+			title_pos = "center",
+			position = { 0, -2 },
+			size = { width = 0.4, height = 11 },
+			zindex = 200,
 		},
 		keys = {
 			["_"] = "jump_split",

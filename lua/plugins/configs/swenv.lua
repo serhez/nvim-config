dd0d4b958881bbc655bca822d0ad3eeddd84ca13
Sergio Hernandez <contact.sergiohernandez@gmail.com>
2025-06-21 12:36:30 +0200

    feat(swenv): more robust envs dir detection

--- lua/plugins/configs/swenv.lua
+++ lua/plugins/configs/swenv.lua
@@ -21,16 +21,44 @@ function M.config()
 	require("swenv").setup({
 		-- attempt to auto create and set a venv from dependencies
 		auto_create_venv = false,
+
 		-- Should return a list of tables with a `name` and a `path` entry each
 		-- Gets the argument `venvs_path` set below
 		-- By default just lists the entries in `venvs_path`
 		get_venvs = function(venvs_path)
-			local global_venvs = require("swenv.api").get_venvs(venvs_path)
-			local workspace_venvs = require("swenv.api").get_venvs(vim.fn.getcwd() .. "/venv")
-			return vim.tbl_deep_extend("force", global_venvs, workspace_venvs)
+			-- Global dir (my conda config)
+			local all_venvs = require("swenv.api").get_venvs(venvs_path)
+
+			-- Just in case
+			local other_venvs = require("swenv.api").get_venvs("~/.envs")
+			all_venvs = vim.tbl_deep_extend("force", all_venvs, other_venvs)
+
+			-- Just in case
+			other_venvs = require("swenv.api").get_venvs("~/venvs")
+			all_venvs = vim.tbl_deep_extend("force", all_venvs, other_venvs)
+
+			-- Just in case
+			other_venvs = require("swenv.api").get_venvs("~/.venvs")
+			all_venvs = vim.tbl_deep_extend("force", all_venvs, other_venvs)
+
+			-- Regular venvs
+			other_venvs = require("swenv.api").get_venvs(vim.fn.getcwd() .. "/venv")
+			all_venvs = vim.tbl_deep_extend("force", all_venvs, other_venvs)
+
+			-- uv
+			other_venvs = require("swenv.api").get_venvs(vim.fn.getcwd() .. "/.venv")
+			all_venvs = vim.tbl_deep_extend("force", all_venvs, other_venvs)
+
+			-- pixi
+			other_venvs = require("swenv.api").get_venvs(vim.fn.getcwd() .. "/.pixi/envs")
+			all_venvs = vim.tbl_deep_extend("force", all_venvs, other_venvs)
+
+			return all_venvs
 		end,
+
 		-- Path passed to `get_venvs`
 		venvs_path = vim.fn.expand("~/envs"),
+
 		-- Something to do after setting an environment, for example call vim.cmd.LspRestart
 		post_set_venv = function(venv)
 			vim.g.active_venv = venv.name

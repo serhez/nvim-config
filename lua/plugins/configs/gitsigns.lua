8110b2645d0015cb4deb2e484255788939cc212c
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2025-07-04 15:00:26 +0200

    feat(git): mapping for toggling inline word diff

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -10,6 +10,13 @@ function M.init()
 
 		{ "<leader>ga", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Author (line)" },
 		{ "<leader>gA", "<cmd>Gitsigns blame<cr>", desc = "Author (buffer)" },
+		{
+			"<leader>gi",
+			function()
+				require("gitsigns").toggle_word_diff()
+			end,
+			desc = "Toggle inline diff",
+		},
 		{ "<leader>gR", "<cmd>Gitsigns reset_buffer<cr>", desc = "Revert buffer" },
 		{ "<leader>gh", "<cmd>Gitsigns preview_hunk<cr>", desc = "Preview hunk" },
 		{ "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", desc = "Revert hunk" },
@@ -82,7 +89,7 @@ function M.config()
 		},
 		trouble = true,
 		gh = true,
-		word_diff = true,
+		word_diff = false,
 		diff_opts = { internal = true },
 	})
 end

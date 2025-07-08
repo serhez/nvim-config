4658549a8d49e88a7e1991162ebd1d03a1ae1a29
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2025-07-08 09:58:25 +0200

    fix(tmux): remove tmux auto-rename integration

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -112,16 +112,16 @@ function M.setup()
 	})
 
 	-- Update tmux window name on VimEnter and VimLeave
-	vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
-		callback = function()
-			if vim.env.TMUX_PLUGIN_MANAGER_PATH then
-				vim.uv.spawn(
-					vim.env.TMUX_PLUGIN_MANAGER_PATH .. "/tmux-window-name/scripts/rename_session_windows.py",
-					{}
-				)
-			end
-		end,
-	})
+	-- vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
+	-- 	callback = function()
+	-- 		if vim.env.TMUX_PLUGIN_MANAGER_PATH then
+	-- 			vim.uv.spawn(
+	-- 				vim.env.TMUX_PLUGIN_MANAGER_PATH .. "/tmux-window-name/scripts/rename_session_windows.py",
+	-- 				{}
+	-- 			)
+	-- 		end
+	-- 	end,
+	-- })
 end
 
 return M

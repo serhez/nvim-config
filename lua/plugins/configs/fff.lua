c8e13cbca2356cf447deb6559e50f599c17f5f2a
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2025-08-22 13:11:54 +0200

    chore(fff): adapt to breaking changes

--- lua/plugins/configs/fff.lua
+++ lua/plugins/configs/fff.lua
@@ -20,16 +20,18 @@ function M.config()
 	local icons = require("icons")
 
 	require("fff").setup({
-		width = 1.0, -- Window width as fraction of screen
-		height = 0.8, -- Window height as fraction of screen
 		prompt = icons.arrow.right_short_thick .. " ",
-		preview = {
-			enabled = true,
-			width = 0.6,
-		},
 		title = "Files", -- Window title
-		max_results = 60, -- Maximum search results to display
+		max_results = 100, -- Maximum search results to display
 		max_threads = 4, -- Maximum threads for fuzzy search
+
+		layout = {
+			height = 0.8,
+			width = 1.0,
+			prompt_position = "bottom", -- or 'top'
+			preview_position = "right", -- or 'left', 'right', 'top', 'bottom'
+			preview_size = 0.6,
+		},
 
 		keymaps = {
 			close = "<Esc>",

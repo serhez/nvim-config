6869ab536e7bb7b112921b0245b704d0dfb16ae1
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2025-10-05 14:29:32 +0200

    fix: update buffer manager config to comply with plugin changes

--- lua/plugins/configs/buffer-manager.lua
+++ lua/plugins/configs/buffer-manager.lua
@@ -6,35 +6,10 @@ local M = {
 	event = "VeryLazy",
 }
 
-function M.init()
-	require("mappings").register({
-		{
-			"<leader>;",
-			function()
-				require("buffer_manager.ui").toggle_persistent_menu()
-			end,
-			desc = "Buffer manager",
-		},
-	})
-end
-
 function M.config()
 	require("buffer_manager").setup({
-		select_menu_item_commands = {
-			{
-				key = "<CR>",
-				command = "edit",
-			},
-			{
-				key = ";",
-				command = "edit",
-			},
-		},
-		focus_alternate_buffer = true,
-		short_file_names = true,
-		short_term_names = true,
-		order_buffers = "lastused",
-		show_indicators = "after",
+		hl_open = "DiagnosticVirtualTextHint",
+		hl_delete = "DiagnosticVirtualTextError",
 	})
 end
 

1c3b92eee10016b8e3d114e4d14f0733e491f7f2
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2025-10-27 10:20:55 +0100

    chore(ui): allow colorscheme to define `hlargs` hls

--- lua/plugins/configs/hlargs.lua
+++ lua/plugins/configs/hlargs.lua
@@ -13,14 +13,9 @@ function M.config()
 		-- HLArgs: 105
 		hl_priority = 105,
 		extras = {
-			named_parameters = { link = "Hlargs" },
 			unused_args = { link = "Comment" },
 		},
 	})
-
-	-- Function parameters highlighting (used by hlargs)
-	-- vim.cmd("autocmd ColorScheme * highlight! link Hlargs TSParameter")
-	-- vim.cmd("highlight! link Hlargs TSParameter")
 end
 
 return M

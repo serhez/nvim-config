224dc8b292ed57cb55fd08d9429db85f1da8a4b0
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2025-12-19 17:26:27 +0100

    feat: better custom highlights for `blink.pairs`

--- lua/plugins/configs/blink-pairs.lua
+++ lua/plugins/configs/blink-pairs.lua
@@ -6,7 +6,18 @@ local M = {
 }
 
 function M.config()
-	require("blink.pairs").setup({})
+	require("blink.pairs").setup({
+		highlights = {
+			groups = {
+				"BlinkPairsDepth1",
+				"BlinkPairsDepth2",
+				"BlinkPairsDepth3",
+				"BlinkPairsDepth4",
+				"BlinkPairsDepth5",
+				"BlinkPairsDepth6",
+			},
+		},
+	})
 end
 
 return M

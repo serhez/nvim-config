dbab3134d1555929d26fb3530ba66bea4afd0ce8
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2025-12-27 16:53:47 +0100

    feat(dap): remote Python config

--- lua/plugins/configs/dap-python.lua
+++ lua/plugins/configs/dap-python.lua
@@ -4,17 +4,36 @@ local M = {
 }
 
 function M.config()
+	local dap = require("dap")
 	local dap_python = require("dap-python")
 	dap_python.setup(vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python")
-	-- dap_python.test_runner = "pytest"
-	-- table.insert(require("dap").configurations.python, {
-	-- 	type = "python",
-	-- 	request = "launch",
-	-- 	name = "Launch file (external breakpoints)",
-	-- 	program = "${file}",
-	-- 	justMyCode = false,
-	-- 	-- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
-	-- })
+
+	dap.adapters.python_remote = {
+		type = "server",
+		host = "127.0.0.1",
+		port = 5678,
+	}
+
+	-- table.insert(dap.configurations.python, function()
+	-- 	local remote_root = vim.fn.input("Remote codebase path: ")
+	--
+	-- 	-- Cancel if user hits Esc or Enter on empty
+	-- 	if remote_root == "" then
+	-- 		return nil
+	-- 	end
+	--
+	-- 	return {
+	-- 		type = "python_remote",
+	-- 		request = "attach",
+	-- 		name = "Attach Remote (Ask for Path)",
+	-- 		pathMappings = {
+	-- 			{
+	-- 				localRoot = vim.fn.getcwd(),
+	-- 				remoteRoot = remote_root,
+	-- 			},
+	-- 		},
+	-- 	}
+	-- end)
 end
 
 return M

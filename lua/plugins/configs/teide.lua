0b0184ff162042f9c1028a33a7faa41c09eec96a
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2025-12-27 16:53:08 +0100

    feat(colorscheme): teide theme

---
+++ lua/plugins/configs/teide.lua
@@ -0,0 +1,46 @@
+local M = {
+	"serhez/teide.nvim",
+	dev = true,
+	name = "teide.nvim",
+}
+
+function M.config()
+	local tokyonight = require("tokyonight")
+	tokyonight.setup({
+		terminal_colors = true,
+		styles = {
+			-- Style to be applied to different syntax groups
+			-- Value is any valid attr-list value for `:help nvim_set_hl`
+			comments = { italic = true },
+			keywords = { italic = true },
+			functions = {},
+			variables = {},
+			-- Background styles. Can be "dark", "transparent" or "normal"
+			sidebars = "dark", -- style for sidebars, see below
+			floats = "dark", -- style for floating windows
+		},
+		sidebars = {
+			"qf",
+			"help",
+			"dapui_scopes",
+			"dapui_watches",
+			"dapui_stacks",
+			"dapui_breakpoints",
+			"dapui_console",
+			"dap-repl",
+		},
+		plugins = {
+			auto = true,
+		},
+		dim_inactive = false,
+		-- on_highlights = function(hl, c)
+		-- hl.CursorColumn = {
+		-- 	bg = c.bg_dark,
+		-- 	fg = c.fg_dark,
+		-- }
+		-- end,
+	})
+	tokyonight.load()
+end
+
+return M

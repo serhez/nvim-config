c560947841881a578ba6c5dde143a9a57bb70817
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2026-01-04 22:18:32 +0100

    feat: adapt to bento API changes

--- lua/plugins/configs/bento.lua
+++ lua/plugins/configs/bento.lua
@@ -7,10 +7,7 @@ local M = {
 
 function M.config()
 	require("bento").setup({
-		hl_open = "DiagnosticVirtualTextHint",
-		hl_delete = "DiagnosticVirtualTextError",
 		max_open_buffers = 10,
-		show_minimal_menu = false,
 	})
 end
 

17e1e417930f73832deaf82c444be91203691bc6
Sergio Hernández Gutiérrez <contact.sergiohernandez@gmail.com>
2026-01-26 20:15:48 +0100

    feat: adapt to `bento` new API

--- lua/plugins/configs/bento.lua
+++ lua/plugins/configs/bento.lua
@@ -8,7 +8,47 @@ local M = {
 function M.config()
 	require("bento").setup({
 		max_open_buffers = 10,
+		map_last_accessed = false,
 	})
+
+	local api = require("bento.api")
+
+	-- Register menu keymaps
+	api.register_expand_key(";")
+	api.register_last_buffer_key(";")
+	api.register_collapse_key("<Esc>")
+	api.register_prev_page_key("[")
+	api.register_next_page_key("]")
+
+	-- Register actions
+	api.register_action("open", {
+		key = "<CR>",
+		action = api.actions.open,
+		hl = "DiagnosticVirtualTextHint",
+	})
+	api.register_action("delete", {
+		key = "<BS>",
+		action = api.actions.delete,
+		hl = "DiagnosticVirtualTextError",
+	})
+	api.register_action("vsplit", {
+		key = "|",
+		action = api.actions.vsplit,
+		hl = "DiagnosticVirtualTextInfo",
+	})
+	api.register_action("split", {
+		key = "_",
+		action = api.actions.split,
+		hl = "DiagnosticVirtualTextInfo",
+	})
+	api.register_action("lock", {
+		key = "*",
+		action = api.actions.lock,
+		hl = "DiagnosticVirtualTextWarn",
+	})
+
+	-- Set default action
+	api.set_default_action("open")
 end
 
 return M
